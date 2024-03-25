da4b09ae17dca88a0956f2d69f491e5df08f8701
Vinicius <vinigugelmin@gmail.com>
2024-03-25 15:45:46 -0300

    feat: Movie model added

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,8 @@
 
 h
 8src/main/java/com/viniciusgugelmin/application/Main.java,9\9\99f342244be866aee0e5847be0562eaf7fc01f33
+j
+:src/main/java/com/viniciusgugelmin/domain/model/Movie.java,4\7\47ff764fe36a53ea345f07e3322cf601ab8f5b92
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
---
+++ src/main/java/com/viniciusgugelmin/domain/model/Movie.java
@@ -0,0 +1,21 @@
+package main.java.com.viniciusgugelmin.domain.model;
+
+import java.time.Year;
+import java.util.Date;
+
+public class Movie {
+    private String imdbID;
+    private String Title;
+    private Year Year;
+    private Date Released;
+    private Double Runtime;
+    private String Genre;
+    /*private String Director;*/
+    /*private String Writer;*/
+    /*private String Actors;*/
+    private String Plot;
+    private String Language;
+    private String Country;
+    private String Awards;
+    private String Poster;
+}
