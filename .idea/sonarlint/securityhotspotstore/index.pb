08b7c94afb0486af49a36509257caebcde7f9580
Vinicius <vinigugelmin@gmail.com>
2024-03-25 15:47:18 -0300

    feat: Country model added

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -6,3 +6,5 @@ j
 :
 
 .gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+l
+<src/main/java/com/viniciusgugelmin/domain/model/Country.java,6\0\60491bc9c44a74d3575b79eb9de5fc1ff8de6105
---
+++ src/main/java/com/viniciusgugelmin/domain/model/Country.java
@@ -0,0 +1,17 @@
+package main.java.com.viniciusgugelmin.domain.model;
+
+public class Country {
+    private String name;
+
+    public Country(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
